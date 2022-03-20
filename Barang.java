3657f178ebab5053dcd08177c0069bed70b67820
Aditia <97042574+Ashuraaaa@users.noreply.github.com>
2022-03-21 00:19:41 +0800

    Add files via upload

---
+++ Barang.java
@@ -0,0 +1,16 @@
+package JavaKasir;
+public class Barang {
+int id;
+String namaBarang;
+int stockBarang, hargaBarang, terjual;
+    
+    public Barang(int id, String namaBarang, int stockBarang, int hargaBarang, int terjual){
+        this.id = id;
+        this.namaBarang  = namaBarang;
+        this.stockBarang = stockBarang;
+        this.hargaBarang = hargaBarang;
+        this.terjual = terjual;
+    }          
+}
+    
+
